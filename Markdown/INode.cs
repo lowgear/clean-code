f70756ee1b9d548f1d91e97bdebf079f2285b3ea
Исламов Айдар Альбертович <lowgear1000@gmail.com>
2017-11-17 21:46:42 +0500

    Structure of code built.

---
+++ Markdown/INode.cs
@@ -0,0 +1,26 @@
+﻿namespace Markdown
+{
+    public interface INode
+    {
+    }
+
+    class TagNode : INode
+    {
+        public int Length { get; }
+
+        public TagNode(int length)
+        {
+            Length = length;
+        }
+    }
+
+    class TextNode : INode
+    {
+        public string Text { get; }
+
+        public TextNode(string text)
+        {
+            Text = text;
+        }
+    }
+}
--- Markdown/Md.cs
+++ Markdown/Md.cs
@@ -1,17 +1,69 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace Markdown
 {
-	public class Md
-	{
-		public string RenderToHtml(string markdown)
-		{
-			return markdown; //TODO
-		}
-	}
+    public class Md
+    {
+        private readonly Dictionary<int, string> underscoresLengthToTag = new Dictionary<int, string>();
 
-	[TestFixture]
-	public class Md_ShouldRender
-	{
-	}
+        public Md(params string[] tags)
+        {
+            for (int i = 0; i < tags.Length; i++)
+            {
+                var tag = tags[i];
+                underscoresLengthToTag[i + 1] = tag;
+            }
+        }
+
+        public string RenderToHtml(string markdown)
+        {
+            var nodes = SplitToUnderscoresSequencesAndText(markdown);
+            var htmlTokens = TranslateMarkdownTokensToHtmlTokens(nodes);
+            var html = string.Join("", htmlTokens);
+            return html;
+        }
+
+        private static List<string> TranslateMarkdownTokensToHtmlTokens(IEnumerable<INode> nodes)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IEnumerable<INode> SplitToUnderscoresSequencesAndText(string markdown)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [TestFixture]
+    public class Md_ShouldRender
+    {
+        private Md md;
+
+        [SetUp]
+        public void SetUp()
+        {
+            md = new Md("em", "strong");
+        }
+
+
+        [Test]
+        public void LeaveUntouched_TextWithoutUnderscores()
+        {
+            CheckMdOn("no underscores", "no underscores");
+        }
+
+        [Test]
+        public void SingleUnderscores_ShouldBeReplacedWithTag()
+        {
+            CheckMdOn("_text_", "<em>text</em>");
+        }
+
+        private void CheckMdOn(string subject, string expected)
+        {
+            md.RenderToHtml(subject).Should().Be(expected);
+        }
+    }
 }
